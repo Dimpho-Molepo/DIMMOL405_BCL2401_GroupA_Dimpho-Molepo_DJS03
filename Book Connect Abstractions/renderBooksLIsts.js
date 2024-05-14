d469e2089742bef1fb63479871d568dbcf78bf37
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-14 15:46:23 +0200

    Created handleSearchFormSubmit for when the search form is submitted

--- Book Connect Abstractions/renderBooksLIsts.js
+++ Book Connect Abstractions/renderBooksLIsts.js
@@ -99,3 +99,69 @@ function setupEventListeners() {
     })
 }
 
+function handleSearchFormSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const filters = Object.fromEntries(formData)
+    const result = []
+
+    for (const book of books) {
+        let genreMatch = filters.genre === 'any'
+
+        for (const singleGenre of book.genres) {
+            if (genreMatch) break;
+            if (singleGenre === filters.genre) { genreMatch = true }
+        }
+
+        if (
+            (filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase())) && 
+            (filters.author === 'any' || book.author === filters.author) && 
+            genreMatch
+        ) {
+            result.push(book)
+        }
+    }
+
+    page = 1;
+    matches = result
+
+    if (result.length < 1) {
+        elementsFromDOM.dataListMessage.classList.add('list__message_show')
+    } else {
+        elementsFromDOM.dataListMessage.classList.remove('list__message_show')
+    }
+
+    elementsFromDOM.dataListItems.innerHTML = ''
+    const newItems = document.createDocumentFragment()
+
+    for (const { author, id, image, title } of result.slice(0, BOOKS_PER_PAGE)) {
+        const element = document.createElement('button')
+        element.classList = 'preview'
+        element.setAttribute('data-preview', id)
+    
+        element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `
+
+        newItems.appendChild(element)
+    }
+
+    elementsFromDOM.dataListItems.appendChild(newItems)
+    elementsFromDOM.dataListButton.disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 1
+
+    elementsFromDOM.dataListButton.innerHTML = `
+        <span>Show more</span>
+        <span class="list__remaining"> (${(matches.length - (page * BOOKS_PER_PAGE)) > 0 ? (matches.length - (page * BOOKS_PER_PAGE)) : 0})</span>
+    `
+
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    elementsFromDOM.dataSearchOverlay.open = false
+}
