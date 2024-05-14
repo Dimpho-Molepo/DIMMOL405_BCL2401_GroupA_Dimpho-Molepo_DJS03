0ca92906b1fb03add238cd5f0ac4c5571a9a9f1d
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-14 15:44:23 +0200

    Created a renderedBooksList.js and moved the functions renderBooks, showGenre, showAuthors, toggleTheme and setUpEvenetListeners

---
+++ Book Connect Abstractions/renderBooksLIsts.js
@@ -0,0 +1,101 @@
+import { books, authors, genres, BOOKS_PER_PAGE } from './data.js';
+import { elementsFromDOM } from './elements.js';
+
+let page = 1;
+let matches = books;
+
+function renderBooks() {
+  const starting = document.createDocumentFragment();
+
+  for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+    const element = document.createElement('button');
+    element.classList = 'preview';
+    element.setAttribute('data-preview', id);
+
+    element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `;
+
+    starting.appendChild(element);
+  }
+
+  elementsFromDOM.dataListItems.appendChild(starting);
+}
+
+function showGenre() {
+  const genreHtml = document.createDocumentFragment();
+  const firstGenreElement = document.createElement('option');
+  firstGenreElement.value = 'any';
+  firstGenreElement.innerText = 'All Genres';
+  genreHtml.appendChild(firstGenreElement);
+
+  for (const [id, name] of Object.entries(genres)) {
+    const element = document.createElement('option');
+    element.value = id;
+    element.innerText = name;
+    genreHtml.appendChild(element);
+  }
+
+  elementsFromDOM.dataSearchGenres.appendChild(genreHtml);
+}
+
+function showAuthors() {
+  const authorsHtml = document.createDocumentFragment();
+  const firstAuthorElement = document.createElement('option');
+  firstAuthorElement.value = 'any';
+  firstAuthorElement.innerText = 'All Authors';
+  authorsHtml.appendChild(firstAuthorElement);
+
+  for (const [id, name] of Object.entries(authors)) {
+    const element = document.createElement('option');
+    element.value = id;
+    element.innerText = name;
+    authorsHtml.appendChild(element);
+  }
+
+  elementsFromDOM.dataSearchAuthors.appendChild(authorsHtml);
+}
+
+function toggleTheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    elementsFromDOM.dataSettingsTheme.value = 'night';
+    document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
+    document.documentElement.style.setProperty('--color-light', '10, 10, 20');
+  } else {
+    elementsFromDOM.dataSettingsTheme.value = 'day';
+    document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
+    document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+  }
+}
+
+function setupEventListeners() {
+    elementsFromDOM.dataSearchCancel.addEventListener('click', () => {
+        elementsFromDOM.dataSearchOverlay.open = false
+    })
+
+    elementsFromDOM.dataSettingsCancel.addEventListener('click', () => {
+        elementsFromDOM.dataSettingsOverlay.open = false
+    })
+
+    elementsFromDOM.dataHeaderSearch.addEventListener('click', () => {
+        elementsFromDOM.dataSearchOverlay.open = true 
+        elementsFromDOM.dataSearchTitle.focus()
+    })
+
+    elementsFromDOM.dataHeaderSettings.addEventListener('click', () => {
+        elementsFromDOM.dataSettingsOverlay.open = true 
+    })
+
+    elementsFromDOM.dataListClose.addEventListener('click', () => {
+        elementsFromDOM.dataListActive.open = false
+    })
+}
+
