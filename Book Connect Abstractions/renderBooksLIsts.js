946f7fda2d7d8366bb34b62c6612ed6e8c28b1c5
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-16 13:59:57 +0200

    Removed some functions that don't necessarily help render books and also added the createPreviewButton function

--- Book Connect Abstractions/renderBooksLIsts.js
+++ Book Connect Abstractions/renderBooksLIsts.js
@@ -1,16 +1,24 @@
-import { books, authors, genres, BOOKS_PER_PAGE } from './data.js';
-import { elementsFromDOM } from './elements.js';
+import { books, authors, BOOKS_PER_PAGE } from "./data.js";
+import { elementsFromDOM } from "./elements.js";
 
 let page = 1;
 let matches = books;
 
+/** Creates a preview button element for a book.
+ *
+ * @param {Object} books - The books object containing the author, id, image, and title properties.
+ * @param {string} books.author - Author of the book.
+ * @param {string} books.id - Unique iID for the author.
+ * @param {string} books.image -The URL of the image to the book.
+ * @param {string} books.title - The title of the book.
+ * @returns {HTMLButtonElement} returns the button element
+ */
 function createPreviewButton({ author, id, image, title }) {
+  const element = document.createElement("button");
+  element.classList = "preview";
+  element.setAttribute("data-preview", id);
 
-    const element = document.createElement('button');
-    element.classList = 'preview';
-    element.setAttribute('data-preview', id);
-
-    element.innerHTML = `
+  element.innerHTML = `
         <img
             class="preview__image"
             src="${image}"
@@ -21,11 +29,11 @@ function createPreviewButton({ author, id, image, title }) {
             <div class="preview__author">${authors[author]}</div>
         </div>
     `;
-    return element
+  return element;
 }
 
 /** Render books on the page with book image, title and author name.
- * 
+ *
  * @returns {void} This function does not return a value.
  */
 function renderBooks() {
@@ -36,222 +44,75 @@ function renderBooks() {
         starting.appendChild(element);
     }
 
-  elementsFromDOM.dataListItems.appendChild(starting);
-}
-
-/** Creates a options for each genre in the genres object, 
- * and appends it to the DOM.
- * 
- * @returns {void} This function does not return a value.
- */
-function showGenre() {
-  const genreHtml = document.createDocumentFragment();
-  const firstGenreElement = document.createElement('option');
-  firstGenreElement.value = 'any';
-  firstGenreElement.innerText = 'All Genres';
-  genreHtml.appendChild(firstGenreElement);
-
-  for (const [id, name] of Object.entries(genres)) {
-    const element = document.createElement('option');
-    element.value = id;
-    element.innerText = name;
-    genreHtml.appendChild(element);
-  }
-
-  elementsFromDOM.dataSearchGenres.appendChild(genreHtml);
-}
-
-/** Creates a options for each author in the authors object, 
- * and appends it to the DOM
- * 
- * @returns {void} This function does not return a value.
- */
-function showAuthors() {  
-    const authorsHtml = document.createDocumentFragment();
-    const firstAuthorElement = document.createElement('option');
-    firstAuthorElement.value = 'any';
-    firstAuthorElement.innerText = 'All Authors';
-    authorsHtml.appendChild(firstAuthorElement);
-
-    for (const [id, name] of Object.entries(authors)) {
-        const element = document.createElement('option');
-        element.value = id;
-        element.innerText = name;
-        authorsHtml.appendChild(element);
-    }
-
-    elementsFromDOM.dataSearchAuthors.appendChild(authorsHtml);
-}
-
-/** Toggle between the dark and light theme for the page
- * 
- * @returns {void} This function does not return a value.
- */
-function toggleTheme() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        elementsFromDOM.dataSettingsTheme.value = 'night';
-        document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
-        document.documentElement.style.setProperty('--color-light', '10, 10, 20');
-    } else {
-        elementsFromDOM.dataSettingsTheme.value = 'day';
-        document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
-        document.documentElement.style.setProperty('--color-light', '255, 255, 255');
-    }
-}
-
-/** Setup the several event listeners for elements in the DOM
- * 
- * @returns {void} This function does not return a value.
- */
-function setupEventListeners() {
-    elementsFromDOM.dataSearchCancel.addEventListener('click', () => {
-        elementsFromDOM.dataSearchOverlay.open = false
-    })
-
-    elementsFromDOM.dataSettingsCancel.addEventListener('click', () => {
-        elementsFromDOM.dataSettingsOverlay.open = false
-    })
-
-    elementsFromDOM.dataHeaderSearch.addEventListener('click', () => {
-        elementsFromDOM.dataSearchOverlay.open = true 
-        elementsFromDOM.dataSearchTitle.focus()
-    })
-
-    elementsFromDOM.dataHeaderSettings.addEventListener('click', () => {
-        elementsFromDOM.dataSettingsOverlay.open = true 
-    })
-
-    elementsFromDOM.dataListClose.addEventListener('click', () => {
-        elementsFromDOM.dataListActive.open = false
-    })
-}
-
-/** Handles the submission of the search form and gets the relevant searches displayed on the page 
- * 
- * @param {Event} event - The form submission event object.
- * @returns {void} This function does not return a value.
- */
-function handleSearchFormSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const filters = Object.fromEntries(formData)
-    const result = []
-
-    for (const book of books) {
-        let genreMatch = filters.genre === 'any'
-
-        for (const singleGenre of book.genres) {
-            if (genreMatch) break;
-            if (singleGenre === filters.genre) { genreMatch = true }
-        }
-
-        if (
-            (filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase())) && 
-            (filters.author === 'any' || book.author === filters.author) && 
-            genreMatch
-        ) {
-            result.push(book)
-        }
-    }
-
-    page = 1;
-    matches = result
-
-    if (result.length < 1) {
-        elementsFromDOM.dataListMessage.classList.add('list__message_show')
-    } else {
-        elementsFromDOM.dataListMessage.classList.remove('list__message_show')
-    }
-
-    elementsFromDOM.dataListItems.innerHTML = ''
-    const newItems = document.createDocumentFragment()
-
-    for (const { author, id, image, title } of result.slice(0, BOOKS_PER_PAGE)) {
-        const element = createPreviewButton({ author, id, image, title });
-        newItems.appendChild(element)
-    }
-
-    elementsFromDOM.dataListItems.appendChild(newItems)
-    elementsFromDOM.dataListButton.disabled = (matches.length - (page * BOOKS_PER_PAGE)) < 1
-
-    elementsFromDOM.dataListButton.innerHTML = `
-        <span>Show more</span>
-        <span class="list__remaining"> (${(matches.length - (page * BOOKS_PER_PAGE)) > 0 ? (matches.length - (page * BOOKS_PER_PAGE)) : 0})</span>
-    `
-
-    window.scrollTo({top: 0, behavior: 'smooth'});
-    elementsFromDOM.dataSearchOverlay.open = false
+    elementsFromDOM.dataListItems.appendChild(starting);
 }
 
 /** Handles the click event for the list button and displays the corresponding list items.
- * 
+ *
  * @returns {void} This function does not return a value.
  */
 function handleListButtonClick() {
-    const fragment = document.createDocumentFragment()
-    
-    for (const { author, id, image, title } of matches.slice(page * BOOKS_PER_PAGE, (page + 1) * BOOKS_PER_PAGE)) {
-        const element = createPreviewButton({ author, id, image, title });
-        fragment.appendChild(element)
-    }
-
-    elementsFromDOM.dataListItems.appendChild(fragment)
-    page += 1
+    renderBooks();
+    page += 1;
+    showMoreBooksButton();
 }
 
 /** Handles the click event for the book list items and displays the book's info
- * 
- * @param {Event} event - The form submission event object.
- * @returns {void} This function does not return a value. event 
- */
-function handleListItemsClick(event) {
-    const pathArray = Array.from(event.path || event.composedPath())
-    let active = null
-
-    for (const node of pathArray) {
-        if (active) break
-
-        if (node?.dataset?.preview) {
-            let result = null
-    
-            for (const singleBook of books) {
-                if (result) break;
-                if (singleBook.id === node?.dataset?.preview) result = singleBook
-            } 
-        
-            active = result 
-        }
-    }
-    
-    if (active) {
-        elementsFromDOM.dataListActive.open = true
-        elementsFromDOM.dataListBlur.src = active.image
-        elementsFromDOM.dataListImage.src = active.image
-        elementsFromDOM.dataListTitle.innerText = active.title
-        elementsFromDOM.dataListSubtitle.innerText = `${authors[active.author]} (${new Date(active.published).getFullYear()})`
-        elementsFromDOM.dataListDescricption.innerText = active.description
-    }
-}
-
-/** Handles the form submission for the settings form and updates the theme accordingly.
  *
  * @param {Event} event - The form submission event object.
  * @returns {void} This function does not return a value.
  */
-function handleSettingsFormSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const { theme } = Object.fromEntries(formData)
-    
-    if (theme === 'night') {
-        document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
-        document.documentElement.style.setProperty('--color-light', '10, 10, 20');
-    } else {
-        document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
-        document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+function handleListItemsClick(event) {
+    const pathArray = Array.from(event.path || event.composedPath());
+    let active = null;
+
+    for (const node of pathArray) {
+        if (active) break;
+
+        if (node?.dataset?.preview) {
+            let result = null;
+
+            for (const singleBook of books) {
+                if (result) break;
+                if (singleBook.id === node?.dataset?.preview) result = singleBook;
+            }
+
+            active = result;
+        }
     }
 
-    elementsFromDOM.dataSettingsOverlay.open = false
+    if (active) {
+        elementsFromDOM.dataListActive.open = true;
+        elementsFromDOM.dataListBlur.src = active.image;
+        elementsFromDOM.dataListImage.src = active.image;
+        elementsFromDOM.dataListTitle.innerText = active.title;
+        elementsFromDOM.dataListSubtitle.innerText = `${authors[active.author]} (${new Date(active.published).getFullYear()})`;
+        elementsFromDOM.dataListDescricption.innerText = active.description;
+    }
 }
 
-export { renderBooks, showGenre, showAuthors, toggleTheme, setupEventListeners, handleSearchFormSubmit, handleListButtonClick, handleListItemsClick, handleSettingsFormSubmit };
+/** This function display the remaining books in list button
+ *
+ * @returns {void} This function does not return a value.
+ */
+function showMoreBooksButton() {
+    elementsFromDOM.dataListButton.innerText = `Show more (${ matches.length - page * BOOKS_PER_PAGE})`;
+    elementsFromDOM.dataListButton.disabled = matches.length - page * BOOKS_PER_PAGE <= 0;
+
+    elementsFromDOM.dataListButton.innerHTML = `
+        <span>Show more</span>
+        <span class="list__remaining"> (${
+        matches.length - page * BOOKS_PER_PAGE > 0
+            ? matches.length - page * BOOKS_PER_PAGE
+            : 0
+        })</span>
+    `;
+}
+
+export {
+  renderBooks,
+  handleListButtonClick,
+  handleListItemsClick,
+  showMoreBooksButton,
+  createPreviewButton,
+};
