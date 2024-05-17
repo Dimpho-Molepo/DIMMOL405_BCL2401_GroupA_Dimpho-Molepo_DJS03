263918fcd582ca0b3c5f35084f6a0b9858a82416
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-17 12:49:08 +0200

    Fixed some indentation issues

--- Book Connect Abstractions/searchForm.js
+++ Book Connect Abstractions/searchForm.js
@@ -1,83 +1,80 @@
-import { books, authors, genres } from './data.js';
-import { elementsFromDOM } from './elements.js';
-import { showMoreBooksButton, renderBooks } from './renderBooksLIsts.js'
+import { books, authors, genres } from "./data.js";
+import { elementsFromDOM } from "./elements.js";
+import { showMoreBooksButton, renderBooks } from "./renderBooksLists.js";
 
 let matches = books;
 
-/** Creates options in the the search form. 
- * 
- * @param {object} authorOrGenreArray 
- * @param {HTMLElement} elementDOM 
- * @param {string} optionString 
+/** Creates options in the the search form.
+ *
+ * @param {object} authorOrGenreArray
+ * @param {HTMLElement} elementDOM
+ * @param {string} optionString
  * @returns {void} This function does not return a value.
  */
-function createOptions(authorOrGenreArray, optionString, elementDOM) {  
-    const fragment = document.createDocumentFragment();
-    const optionElement = document.createElement('option');
-    optionElement.value = 'any';
-    optionElement.innerText = optionString;
-    fragment.appendChild(optionElement);
+function createOptions(authorOrGenreArray, optionString, elementDOM) {
+  const fragment = document.createDocumentFragment();
+  const optionElement = document.createElement("option");
+  optionElement.value = "any";
+  optionElement.innerText = optionString;
+  fragment.appendChild(optionElement);
 
-    for (const [id, name] of Object.entries(authorOrGenreArray)) {
-        const element = document.createElement('option');
-        element.value = id;
-        element.innerText = name;
-        fragment.appendChild(element);
-    }
+  for (const [id, name] of Object.entries(authorOrGenreArray)) {
+    const element = document.createElement("option");
+    element.value = id;
+    element.innerText = name;
+    fragment.appendChild(element);
+  }
 
-    elementDOM.appendChild(fragment);
+  elementDOM.appendChild(fragment);
 }
 
-createOptions(authors, 'All Authors', elementsFromDOM.dataSearchAuthors)
-createOptions(genres,'All Genres',elementsFromDOM.dataSearchGenres)
+createOptions(authors, "All Authors", elementsFromDOM.dataSearchAuthors);
+createOptions(genres, "All Genres", elementsFromDOM.dataSearchGenres);
 
-function filterBooks(bookFilter){
-    const result = []
-    
-    for (const book of books) {
-        let genreMatch = bookFilter.genre === 'any'
+function filterBooks(bookFilter) {
+  const result = [];
 
-        for (const singleGenre of book.genres) {
-            if (genreMatch) break;
-            if (singleGenre === bookFilter.genre) { genreMatch = true }
-        }
+  for (const book of books) {
+    let genreMatch = bookFilter.genre === "any";
 
-        if (
-            (bookFilter.title.trim() === '' || book.title.toLowerCase().includes(bookFilter.title.toLowerCase())) && 
-            (bookFilter.author === 'any' || book.author === bookFilter.author) && 
-            genreMatch
-        ) {
-            result.push(book)
-        }
+    for (const singleGenre of book.genres) {
+      if (genreMatch) break;
+      if (singleGenre === bookFilter.genre) {
+        genreMatch = true;
+      }
     }
 
-    return result
+    if ((bookFilter.title.trim() === "" || book.title.toLowerCase().includes(bookFilter.title.toLowerCase())) && 
+        (bookFilter.author === "any" || book.author === bookFilter.author) &&genreMatch) {
+      result.push(book);
+    }
+  }
+
+  return result;
 }
 
-
-/** Handles the submission of the search form and gets the relevant searches displayed on the page 
- * 
+/** Handles the submission of the search form and gets the relevant searches displayed on the page
+ *
  * @param {Event} event - The form submission event object.
  * @returns {void} This function does not return a value.
- */
+*/
 export function handleSearchFormSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const filters = Object.fromEntries(formData)
-   
-    matches = filterBooks(filters)
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const filters = Object.fromEntries(formData);
 
-    if (matches.length < 1) {
-        elementsFromDOM.dataListMessage.classList.add('list__message_show')
-    } else {
-        elementsFromDOM.dataListMessage.classList.remove('list__message_show')
-    }
+  matches = filterBooks(filters);
 
-    elementsFromDOM.dataListItems.innerHTML = ''
-    renderBooks();
-    
-    showMoreBooksButton();
-    window.scrollTo({top: 0, behavior: 'smooth'});
-    elementsFromDOM.dataSearchOverlay.open = false
+  if (matches.length < 1) {
+    elementsFromDOM.dataListMessage.classList.add("list__message_show");
+  } else {
+    elementsFromDOM.dataListMessage.classList.remove("list__message_show");
+  }
+
+  elementsFromDOM.dataListItems.innerHTML = "";
+  renderBooks();
+
+  showMoreBooksButton(page, matches);
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  elementsFromDOM.dataSearchOverlay.open = false;
 }
-
