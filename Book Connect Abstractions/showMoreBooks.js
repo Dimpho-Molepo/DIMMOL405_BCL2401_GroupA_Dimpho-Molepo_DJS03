171d2aab5e428bf2491a2ac6b69af6bc184e10f3
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-05-19 15:48:11 +0200

    Added some comments

--- Book Connect Abstractions/showMoreBooks.js
+++ Book Connect Abstractions/showMoreBooks.js
@@ -1,10 +1,15 @@
 import { books, BOOKS_PER_PAGE } from "./data.js";
 import { elementsFromDOM } from "./elements.js";
 
-/** This function display the remaining books in list button
+let page = 1;
+let matches = books;
+
+/** This function display the remaining books on list button
  *
+ * @param {number} page - The current page number.
+ * @param {Array<Object>} matches - The array of books that match the current filters.
  * @returns {void} This function does not return a value.
- */
+*/
 export function showMoreBooksButton(page, matches) {
     elementsFromDOM.dataListButton.innerText = `Show more (${ matches.length - page * BOOKS_PER_PAGE})`;
     elementsFromDOM.dataListButton.disabled = matches.length - page * BOOKS_PER_PAGE <= 0;
@@ -18,7 +23,4 @@ export function showMoreBooksButton(page, matches) {
         })</span>
     `;
 }
-
-let page = 1;
-let matches = books;
 showMoreBooksButton(page, matches);
